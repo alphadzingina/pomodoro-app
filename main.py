0ae6909f31f65e59cbbcdb5d2f6da7661f3d8c20
alphadzingina <alphaadx6@gmail.com>
2022-05-19 01:45:01 +0100

    minor ui arguments

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ window = Tk()
 window.title("Pomodoro")
 window.configure(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 
