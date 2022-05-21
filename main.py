735a7c4a51a2a4ff928b7580e1b1e76f2da2b2b1
alphadzingina <alphaadx6@gmail.com>
2022-05-22 00:36:00 +0100

    reset timer

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    title_label.config(text="Timer", fg=GREEN, bg=YELLOW)
+    canvas.itemconfig(timer_text, text=f"00:00")
+    checkmark_label.config(text="✔")
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
@@ -25,15 +31,15 @@ def start_timer():
     
     if reps % 8 == 0: #if 8th rep
         count_down(long_break_sec)
-        title_label.config(text="Break", fg=RED, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+        title_label.config(text="Break", fg=RED, bg=YELLOW)
 
     elif reps % 2 == 0: #if 2nd, 4th, 6th rep
         count_down(short_break_sec)
-        title_label.config(text="Break", fg=PINK, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+        title_label.config(text="Break", fg=PINK, bg=YELLOW)
 
     else: #if it's the 1st/3rd/5th/7th rep
         count_down(work_sec)
-        title_label.config(text="Work", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+        title_label.config(text="Work", fg=GREEN, bg=YELLOW)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -43,10 +49,11 @@ def count_down(count):
 
     if count_sec < 10:
         count_sec = f"0{count_sec}"
-
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+
     if count > 0:
-        window.after(1000, count_down, count -1)
+        global timer
+        timer = window.after(1000, count_down, count -1)
     else:
         start_timer()
         mark = ""
@@ -72,7 +79,7 @@ title_label.grid(column=1, row=0)
 start_button = Button(text="Calculate", bg=YELLOW, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0)
+reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 checkmark_label = Label(fg=GREEN, bg=YELLOW)
